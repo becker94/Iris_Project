2804289881453057c424c17d7cd2a7d6041b43b0
Vador <ls4b.vador@gmail.com>
2026-01-21 15:41:39 +0100

    page accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,12 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class AccueilController {
 
-function index (): Response {
-    return new Response ('Bonjour les gens', ); 
+class AccueilController extends AbstractController {
+
+#[Route("/accueil",name : "home")]
+
+public function index(): Response{
+    return $this->render('accueil/index.html.twig');
 }
 }
+
