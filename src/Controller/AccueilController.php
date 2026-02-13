ec498f9c14da581becd2b06711e82115c4836e95
Vador <ls4b.vador@gmail.com>
2026-02-13 08:15:17 +0100

    Modification

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,17 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
-class AccueilController extends AbstractController {
-
-#[Route("/accueil",name : "home")]
-
-public function index(): Response{
-    return $this->render('accueil/index.html.twig');
+class AccueilController extends AbstractController
+{
+    #[Route('/', name: 'app_accueil')]
+    public function index(): Response
+    {
+        return $this->render('accueil/index.html.twig');
+    }
 }
-}
-
--- src/Controller/ComingSoonController.php
+++ src/Controller/ComingSoonController.php
@@ -6,13 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class CatalogueController extends AbstractController
+class ComingSoonController extends AbstractController
 {
-    #[Route('/catalogue', name: 'app_catalogue')]
+    #[Route('/bientot', name: 'app_coming_soon')]
     public function index(): Response
     {
-        return $this->render('catalogue/index.html.twig', [
-            'vehicules' => [],
-        ]);
+        return $this->render('comingsoon/index.html.twig');
     }
 }
